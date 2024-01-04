574a8752c4df31fe2ec7dbd17f48952063457e33
ZIQI-code <zq001@bucknell.edu>
2024-01-04 23:08:09 +0800

    ziqi

--- Alphanet.py
+++ Alphanet.py
@@ -1,5 +1,14 @@
 from jqdatasdk import *
-
-
-
+import numpy as np
+import pandas as pd
+from datetime import time
 auth('18813155136','AlphaNet666')
+
+security = '601127.XSHG'
+print(get_security_info(security).display_name)
+
+df = get_price(security, count = 30, end_date='2023-10-1', frequency='daily',
+               fields=['open', 'close', 'low', 'high', 'volume'],
+               skip_paused=True, fq='none',  panel=False, fill_paused=True)
+
+print(df)
